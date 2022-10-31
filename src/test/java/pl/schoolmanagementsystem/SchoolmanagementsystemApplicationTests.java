ed7a6a694d406185eb659dc7a1ae187f13fd7264
wd11111 <xxwejtixx@gmail.com>
2022-10-31 19:58:04 +0100

    test commit

---
+++ src/test/java/pl/schoolmanagementsystem/SchoolmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.schoolmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

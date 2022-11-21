e3eaa2211454a5cb1dc3329decd884a184554dcb
wd11111 <xxwejtixx@gmail.com>
2022-11-21 17:52:18 +0100

    added tests for StudentMarkServiceTest

--- src/main/java/pl/schoolmanagementsystem/common/mark/dto/MarkWithTwoFields.java
+++ src/main/java/pl/schoolmanagementsystem/common/mark/dto/MarkWithTwoFields.java
@@ -1,10 +1,12 @@
 package pl.schoolmanagementsystem.common.mark.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 public class MarkWithTwoFields {
---
+++ src/test/java/pl/schoolmanagementsystem/student/service/StudentMarkServiceTest.java
@@ -0,0 +1,58 @@
+package pl.schoolmanagementsystem.student.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.schoolmanagementsystem.ControllerSamples;
+import pl.schoolmanagementsystem.common.mark.MarkRepository;
+import pl.schoolmanagementsystem.common.mark.MarkSamples;
+import pl.schoolmanagementsystem.common.mark.dto.MarkAvgDto;
+import pl.schoolmanagementsystem.common.mark.dto.MarkWithTwoFields;
+import pl.schoolmanagementsystem.common.student.StudentRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StudentMarkServiceTest implements ControllerSamples, MarkSamples {
+
+    @Mock
+    private StudentRepository studentRepository;
+
+    @Mock
+    private MarkRepository markRepository;
+
+    @InjectMocks
+    private StudentMarkService studentMarkService;
+
+    @Test
+    void should_return_marks_grouped_by_subject() {
+        Map<String, List<MarkWithTwoFields>> expected = new HashMap<>(
+                Map.of(SUBJECT_BIOLOGY, List.of(createMarkWithTwoFields()),
+                        SUBJECT_HISTORY, List.of(createMarkWithTwoFields2())));
+        when(markRepository.findAllMarksForStudent(anyString()))
+                .thenReturn(List.of(createMarkWithTwoFields(), markWithTwoFields2()));
+
+        Map<String, List<MarkWithTwoFields>> result = studentMarkService.getGroupedMarksBySubject(NAME);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void should_return_all_averages_for_student() {
+        List<MarkAvgDto> expectedResult = List.of(new MarkAvgDto(SUBJECT_BIOLOGY, MARK));
+        when(studentRepository.findIdByEmail(anyString())).thenReturn(ID_1);
+        when(markRepository.findAllAveragesForStudent(ID_1)).thenReturn(expectedResult);
+
+        List<MarkAvgDto> result = studentMarkService.getAverageMarks(NAME);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+}

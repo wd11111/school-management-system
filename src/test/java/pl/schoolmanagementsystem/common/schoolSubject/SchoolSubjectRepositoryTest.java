0b990f5947f0cf31feb71564ad917471e94b890f
wd11111 <xxwejtixx@gmail.com>
2022-11-23 12:44:31 +0100

    added tests for school subject repository

---
+++ src/test/java/pl/schoolmanagementsystem/common/schoolSubject/SchoolSubjectRepositoryTest.java
@@ -0,0 +1,39 @@
+package pl.schoolmanagementsystem.common.schoolSubject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import pl.schoolmanagementsystem.DataJpaTestBase;
+import pl.schoolmanagementsystem.Samples;
+import pl.schoolmanagementsystem.common.schoolSubject.dto.SchoolSubjectDto;
+import pl.schoolmanagementsystem.common.schoolSubject.dto.SubjectAndTeacherOutputDto;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+class SchoolSubjectRepositoryTest extends DataJpaTestBase implements Samples {
+
+    @Autowired
+    private SchoolSubjectRepository schoolSubjectRepository;
+
+    @Test
+    void should_return_all_school_subjects() {
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<SchoolSubjectDto> result = schoolSubjectRepository.findAllSchoolSubjects(pageable);
+
+        assertThat(result).extracting("subjectName").containsAll(List.of("biology", "history"));
+    }
+
+    @Test
+    void should_return_all_taught_subjects_in_school_class() {
+        List<SubjectAndTeacherOutputDto> result = schoolSubjectRepository.findAllSubjectsInSchoolClass("1a");
+
+        assertThat(result).extracting("subject", "teacherName", "teacherSurname")
+                .containsAll(List.of(tuple("biology", "name", "surname"),
+                        tuple("history", "name", "surname")));
+    }
+}
--- src/test/resources/scripts/init_db.sql
+++ src/test/resources/scripts/init_db.sql
@@ -2,7 +2,8 @@ INSERT INTO role
 VALUES ('ROLE');
 
 INSERT INTO app_user
-VALUES ('email', 'password', 'token');
+VALUES ('email', 'password', 'token'),
+       ('email2', 'password', 'token');
 
 INSERT INTO app_user_roles
 VALUES ('email', 'ROLE');
@@ -22,3 +23,19 @@ INSERT INTO mark
 VALUES (1, 4, 3, 'biology'),
        (2, 1, 3, 'biology'),
        (3, 2, 3, 'history');
+
+INSERT INTO teacher
+VALUES (1, 'name', 'surname', 'email2');
+
+INSERT INTO teacher_taught_subjects
+VALUES (1, 'biology'),
+       (1, 'history');
+
+INSERT INTO teacher_in_class
+VALUES (1, 'biology', 1),
+        (2, 'history', 1);
+
+INSERT INTO teacher_in_class_taught_classes
+VALUES (1, '1a'),
+       (2, '1a');
+

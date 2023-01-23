67c6d308a53dffc2eb154aaaafda1495ebf8ca1e
Wojtek <xxwejtixx@gmail.com>
2023-01-23 18:44:26 +0100

    refactored AppUserService

--- src/main/java/pl/schoolmanagementsystem/common/role/AppUserService.java
+++ src/main/java/pl/schoolmanagementsystem/common/role/AppUserService.java
@@ -11,6 +11,11 @@ public class AppUserService {
     public static final String PASSWORD = null;
 
     public static AppUser createAppUser(String email) {
-        return new AppUser(email, PASSWORD, generateToken(), new ArrayList<>());
+        return AppUser.builder()
+                .userEmail(email)
+                .password(PASSWORD)
+                .token(generateToken())
+                .roles(new ArrayList<>())
+                .build();
     }
 }

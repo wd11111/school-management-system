6909e464a8b579b6318cad0f490262ba249cc7cd
Wojtek <xxwejtixx@gmail.com>
2023-01-23 18:44:41 +0100

    fixed SchoolSubjectMapper

--- src/main/java/pl/schoolmanagementsystem/schoolSubject/utils/SchoolSubjectMapper.java
+++ src/main/java/pl/schoolmanagementsystem/schoolSubject/utils/SchoolSubjectMapper.java
@@ -1,11 +1,13 @@
 package pl.schoolmanagementsystem.schoolSubject.utils;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pl.schoolmanagementsystem.common.dto.SchoolSubjectDto;
 import pl.schoolmanagementsystem.common.model.SchoolSubject;
 
 @Mapper(componentModel = "spring")
 public interface SchoolSubjectMapper {
 
+    @Mapping(source = "schoolSubjectDto.subjectName", target = "name")
     SchoolSubject mapDtoToEntity(SchoolSubjectDto schoolSubjectDto);
 }

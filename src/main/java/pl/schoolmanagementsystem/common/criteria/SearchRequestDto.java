3611d0b63aca9b06c59d4841c0578c6666072b19
Wojtek <xxwejtixx@gmail.com>
2023-01-23 19:39:41 +0100

    refactored search request dto

--- src/main/java/pl/schoolmanagementsystem/common/criteria/SearchRequestDto.java
+++ src/main/java/pl/schoolmanagementsystem/common/criteria/SearchRequestDto.java
@@ -5,26 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchRequestDto {
 
-    @NotNull(message = "{not.null.message}")
-    @NotBlank(message = "{not.blank.message}")
     private String column;
-    @NotNull(message = "{not.null.message}")
-    @NotBlank(message = "{not.blank.message}")
     private String value;
-    @NotNull(message = "{not.null.message}")
-    @NotBlank(message = "{not.blank.message}")
     private Operation operation;
-    @NotNull(message = "{not.null.message}")
-    @NotBlank(message = "{not.blank.message}")
     private ValueType valueType;
 
     public enum Operation {

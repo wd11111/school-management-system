c9d11a46332d4dd827754de2db0fedae82db37a1
Wojtek <xxwejtixx@gmail.com>
2023-02-02 21:30:25 +0100

    small refactor

--- src/main/java/pl/schoolmanagementsystem/common/email/config/EmailConfig.java
+++ src/main/java/pl/schoolmanagementsystem/common/email/config/EmailConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
+import pl.schoolmanagementsystem.common.email.service.EmailSender;
 import pl.schoolmanagementsystem.common.email.service.EmailService;
 import pl.schoolmanagementsystem.common.email.service.FakeEmailService;
 
@@ -12,13 +13,14 @@ public class EmailConfig {
 
     @Bean
     @ConditionalOnProperty(name = "use.real.email.sender", havingValue = "true")
-    public EmailService emailService(JavaMailSender mailSender) {
+    public EmailSender emailService(JavaMailSender mailSender) {
         return new EmailService(mailSender);
     }
 
     @Bean
     @ConditionalOnProperty(name = "use.real.email.sender", havingValue = "false")
-    public FakeEmailService fakeEmailService() {
+    public EmailSender fakeEmailService() {
         return new FakeEmailService();
     }
+
 }

a1d29a81261046c29744c3a8fa7d5beefaf7151c
Wojtek <xxwejtixx@gmail.com>
2023-02-13 15:39:39 +0100

    small refactor

--- src/main/java/pl/schoolmanagementsystem/security/dto/EmailDto.java
+++ src/main/java/pl/schoolmanagementsystem/security/dto/EmailDto.java
@@ -3,5 +3,7 @@ package pl.schoolmanagementsystem.security.dto;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-public record EmailDto(@NotNull @NotEmpty String email) {
+public record EmailDto(@NotNull(message = "{not.null.message}")
+                       @NotEmpty (message = "{not.empty.message}")
+                       String email) {
 }

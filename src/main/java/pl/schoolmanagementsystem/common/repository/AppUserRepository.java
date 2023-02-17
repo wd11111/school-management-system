075c7fa0c828e2ebfc87d00c271f2fa9f08bd5ab
Wojtek <xxwejtixx@gmail.com>
2023-02-18 00:04:43 +0100

    added one method after due to AppUser entity refactor

--- src/main/java/pl/schoolmanagementsystem/common/repository/AppUserRepository.java
+++ src/main/java/pl/schoolmanagementsystem/common/repository/AppUserRepository.java
@@ -15,6 +15,6 @@ public interface AppUserRepository extends JpaRepository<AppUser, String> {
     @Query("SELECT ap FROM AppUser ap LEFT JOIN FETCH ap.roles WHERE ap.email=?1")
     Optional<AppUser> findByUserEmail(String email);
 
-    //Optional<AppUser> findByUserEmail(String email);
+    boolean existsByEmail(String email);
 }
 
